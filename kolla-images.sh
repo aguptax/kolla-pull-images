6a79c5473d7d6b76bc328cc06d6b2970be6676b4
Ankit Gupta | Founder Xloud Technologies <153551386+aguptax@users.noreply.github.com>
2025-09-15 13:41:08 +0530

    Update kolla-images.sh

--- kolla-images.sh
+++ kolla-images.sh
@@ -1,103 +1,142 @@
-##############--coreservices
-xavs-toolbox
-fluentd
-cron
-
-mariadb
-mariadb-server
-mariadb-clustercheck
-
-keepalived
-haproxy
-
-horizon
-
-keystone-ssh
-keystone
-keystone-fernet
-
-heat-engine
-heat-api
-heat-api-cfn
-
-neutron-server
-neutron-metadata-agent
-neutron-openvswitch-agent
-neutron-dhcp-agent
-neutron-l3-agent
-
-openvswitch-vswitchd
-openvswitch-db-server
-
-nova-api
-nova-conductor
-nova-scheduler
-nova-compute
-nova-libvirt
-nova-novncproxy
-nova-ssh
-
-placement-api
-
+# Storage services: block, file, object storage
 cinder-api
+cinder-backup
 cinder-scheduler
 cinder-volume
-cinder-backup
-iscsid
-tgtd
+manila-api
+manila-data
+manila-scheduler
+manila-share
 
-glance-api
+# Identity, Secrets, and Access control
+keystone
+keystone-fernet
+keystone-ssh
+barbican-api
+barbican-keystone-listener
+barbican-worker
 
-rabbitmq
-memcached
+# Compute services: VM lifecycle management
+nova-api
+nova-compute
+nova-compute-ironic
+nova-conductor
+nova-libvirt
+nova-novncproxy
+nova-scheduler
+nova-serialproxy
+nova-spicehtml5proxy
+nova-ssh
 
+# Networking services: virtual networks, routing, load balancers
+neutron-dhcp-agent
+neutron-l3-agent
+neutron-metadata-agent
+neutron-openvswitch-agent
+neutron-ovn-agent
+neutron-server
+neutron-sriov-agent
+octavia-api
+octavia-driver-agent
+octavia-health-manager
+octavia-housekeeping
+octavia-worker
 
-##############--OVN
-ovn-controller
-ovn-northd
-ovn-sb-db-server
-ovn-nb-db-server
-#######################################
+# Baremetal provisioning (Ironic, Bifrost)
+bifrost-deploy
+ironic-api
+ironic-conductor
+ironic-inspector
+ironic-neutron-agent
+ironic-prometheus-exporter
+ironic-pxe
 
-
-ceilometer-compute
-ceilometer-central
-ceilometer-notification
-ceilometer-ipmi
-ceilometer-base
-
-gnocchi-api
-gnocchi-metricd
-gnocchi-statsd
-gnocchi-base
-
-aodh-api
-aodh-notifier
-aodh-evaluator
-aodh-listener
-aodh-expirer
-aodh-base
-
-skyline-apiserver
-skyline-console
-
+# Orchestration and Autoscaling
+heat-api
+heat-api-cfn
+heat-engine
+magnum-api
+magnum-conductor
 watcher-api
 watcher-applier
 watcher-engine
 
-masakari-base
+# Availability, clustering, and failover
 masakari-api
 masakari-engine
 masakari-monitors
-hacluster-pacemaker
 hacluster-corosync
-hacluster-base
+hacluster-pacemaker
+hacluster-pacemaker-remote
 hacluster-pcs
+keepalived
 
+# DNS service
+designate-api
+designate-backend-bind9
+designate-central
+designate-mdns
+designate-producer
+designate-sink
+designate-worker
 
+# Placement service
+placement-api
 
-barbican-api
-barbican-keystone-listener
-barbican-worker
-barbican-base
+# Dashboard/UI
+horizon
 
+# Monitoring, logging, and observability
+grafana
+influxdb
+prometheus-alertmanager
+prometheus-blackbox-exporter
+prometheus-cadvisor
+prometheus-elasticsearch-exporter
+prometheus-haproxy-exporter
+prometheus-libvirt-exporter
+prometheus-memcached-exporter
+prometheus-msteams
+prometheus-mtail
+prometheus-mysqld-exporter
+prometheus-node-exporter
+prometheus-openstack-exporter
+prometheus-ovn-exporter
+prometheus-server
+prometheus-v2-server
+opensearch
+opensearch-dashboards
+fluentd
+collectd
+
+# Messaging, DB, caching
+mariadb-clustercheck
+mariadb-server
+proxysql
+rabbitmq
+memcached
+redis
+redis-sentinel
+etcd
+
+# Networking backends (switching, OVN/OVS)
+openvswitch-db-server
+openvswitch-vswitchd
+ovsdpdk
+ovsdpdk-db
+ovsdpdk-vswitchd
+ovn-controller
+ovn-nb-db-server
+ovn-northd
+ovn-sb-db-relay
+ovn-sb-db-server
+
+# Supporting services / infrastructure
+haproxy
+haproxy-ssh
+dnsmasq
+iscsid
+multipathd
+tgtd
+cron
+xavs-toolbox
